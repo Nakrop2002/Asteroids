ee4c7ecfae06da87cda7d04c742d65ef091dbd6c
Are5428 <nakropjob@gmail.com>
2025-05-08 16:33:46 +0700

    -player can shoot -gameover when player collide with asteroids -the asteroids split when the bullet collide with them.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -7,8 +12,41 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    
+    AsteroidField.containers = (updatable)
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+    asteroidField = AsteroidField()
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player1):
+                print("GAME OVER")
+                sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
+                    
         screen.fill("black")
+        for player in drawable:
+            player.draw(screen)
+        dt = time.tick(60)/ 1000
+        
         pygame.display.flip()
 
 if __name__ == "__main__":
